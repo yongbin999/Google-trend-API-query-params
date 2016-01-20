fc43d4879a898c53d83a8f90016ec1f6efe68764
Yong Liang <yongbin999@gmail.com>
2016-01-19 21:00:07 -0500

    refine the script
    refine the script

--- script.sh
+++ script.sh
@@ -1,1 +1,8 @@
-curl --data "ajax=1&cid=actors&geo=US&date=201310" http://www.google.com/trends/topcharts/trendingchart
+
+#!/bin/bash
+
+results=""					# set a local shell variable
+results= curl --data "ajax=1&cid=actors&geo=US&date=201310" http://www.google.com/trends/topcharts/trendingchart
+
+$results >  results.txt
+#
